8fecb4d7b28ce794db0b03a105c7c75ce0899ecf
tim <tfierek357@gmail.com>
2022-10-16 11:07:23 -0400

    init commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Navbar() {
+    return (
+        <nav>
+            <img src="./globe-icon.png" alt="" />
+            <h1 className="navbar-text">my travel journal.</h1>
+        </nav>
+    );
+}
+
+export default Navbar;
---
+++ src/data.tsx
@@ -0,0 +1,40 @@
+const data = [
+    {
+        title: "Mount Fuji",
+        location: "Japan",
+        googleMapsUrl: "https://goo.gl/maps/9PbyS9CtGHyh8WMo6",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "Japan’s Mt. Fuji is an active volcano about 100 kilometers southwest of Tokyo. Commonly called “Fuji-san,” it’s the country’s tallest peak, at 3,776 meters. A pilgrimage site for centuries, it’s considered one of Japan’s 3 sacred mountains, and summit hikes remain a popular activity.",
+        imageUrl: "https://images.unsplash.com/photo-1578637387939-43c525550085?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+    },
+    {
+        title: "Detroit, MI",
+        location: "USA",
+        googleMapsUrl: "https://goo.gl/maps/uoYmCMXy8svfY5Xo8",
+        startDate: "1 May, 2002",
+        endDate: "19 May, 2002",
+        description: "Detroit is the largest city in the midwestern state of Michigan. Near Downtown, the neoclassical Detroit Institute of Arts is famed for the Detroit Industry Murals painted by Diego Rivera, and inspired by the city’s ties to the auto industry, giving it the nickname 'Motor City.'",
+        imageUrl: "https://images.unsplash.com/photo-1568267938179-f50e67de5ffc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1493&q=80"
+    },
+    {
+        title: "Hawaii",
+        location: "USA",
+        googleMapsUrl: "https://goo.gl/maps/htqhr89VunHPZe4g8",
+        startDate: "10 Dec, 2022",
+        endDate: "15 Dec, 2022",
+        description: "The Big Island (officially named Hawaii) is the largest island in the United States' Hawaiian archipelago in the Central Pacific. Its diverse terrain spans colored-sand beaches at Papakolea (green) and Punalu’u (black) to lush rainforest.",
+        imageUrl: "https://images.unsplash.com/photo-1505852679233-d9fd70aff56d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+    },
+    {
+        title: "Wakeboarding Trip",
+        location: "Bahamas",
+        googleMapsUrl: "https://goo.gl/maps/uzYqH2j9584hoT1p7",
+        startDate: "2 Feb, 2023",
+        endDate: "9 Feb, 2023",
+        description: "Wakeboarding can be done all over the islands, with wakeboard operators based in many resorts. Nassau is the island's capital, but its waters can be choppy. It's best to take the short hop over to Paradise Island, where there is a calm, protected bay with plenty of space that is perfect for wakeboarding.",
+        imageUrl: "https://images.unsplash.com/photo-1484634410561-aa51b83c27d6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+    }
+]
+
+export default data;
